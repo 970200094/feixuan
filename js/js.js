d07733278710d52077c31936e9b1b85842fa176a
wxd123666 <978690943@qq.com>
2015-08-25 18:45:44 +0800

    ee

---
+++ js/js.js
@@ -0,0 +1,29 @@
+
+window.onload=function(){
+	var ocont=document.querySelector(".continer");
+	var ovide=document.querySelector(".video");
+	var odown=document.querySelector(".down");
+	odown.style.opacity="1";
+	odown.style.right="0";
+	 odown.style.transform="rotate(0deg)";
+	 odown.style.WebkitTransform="rotate(0deg)";
+	 odown.style.mstransform="rotate(0deg)";
+	 odown.style.MozTransform="rotate(0deg)";
+	
+	ovide.style.opacity="1";
+	ovide.style.left="0";
+	 ovide.style.transform="rotateY(0deg)";
+	 ovide.style.WebkitTransform="rotateY(0deg)";
+	 ovide.style.mstransform="rotateY(0deg)";
+	 ovide.style.MozTransform="rotateY(0deg)";
+	
+	ocont.style.opacity="1";
+	 ocont.style.transform="rotateY(0deg)";
+	 ocont.style.WebkitTransform="rotateY(0deg)";
+	 ocont.style.mstransform="rotateY(0deg)";
+	 ocont.style.MozTransform="rotateY(0deg)";
+	 
+	
+	odive.style.opacity="1";
+	odive.style.left="0";
+	}
